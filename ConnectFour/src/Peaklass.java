33babff50c8a07f3c7489c02374d3935a162e500
Nora Anete Müller <nora.anete.muller@gmail.com>
2020-03-31 17:07:25 +0300

    Update Peaklass.java

--- ConnectFour/src/Peaklass.java
+++ ConnectFour/src/Peaklass.java
@@ -1,26 +1,58 @@
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Peaklass {
     public static void main(String[] args) {
+        //mängu selgitav tekst
+        System.out.println("Tere tulemast mängima mängu 'Connect Four'! ");
+        System.out.println("Oma käigu ajal pead valima, millisest veerust oma nuppu soovid sisestada. Seejärel teeb seda sama sinu vastaseks olev arvuti." +
+                " Mängu eesmärgiks on olla esimene, kes moodustab horisontaalse, vertikaalse või diagonaalse joone neljast oma nupust.");
+        System.out.println("Alustamiseks pead sisestama oma nime ja sümboli, millega sooviksid mängida, näiteks '⚫'." +
+                " Seejärel sisesta soovitud mängulaua suurused.");
+        System.out.println("Head mängimist!");
+
+        //kasutajaga suhtlemine
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Sisestage oma nimi: ");
+        String mangijaNimi = scanner.next();
+        System.out.println("Sisestage sümbol, millega tahaksite mängida: ");
+        String mangijaNupp = scanner.next();
+        System.out.println("Sisestage soovitud mängulaua ridade arv: ");
+        int ridadeArv = scanner.nextInt();
+        System.out.println("Sisestage soovitud mängulaua veergude arv: ");
+        int veergudeArv = scanner.nextInt();
+
         //loome mängijad
         Mangija arvuti = new Mangija("Arvuti", "◙");
-        Mangija inimene = new Mangija("Nonna", "⚫");
+        Mangija inimene = new Mangija(mangijaNimi, mangijaNupp);
+
         //loome mängulaua
-        Maatriks laud = new Maatriks(6,7);
+        Maatriks laud = new Maatriks(ridadeArv,veergudeArv);
+        System.out.println("Loodud mängulaud: ");
         laud.looTuhiMangulaud();
 
-        //mäng käib
-        laud.lisaNupp(1, inimene.getNupp());
-        laud.valjastaMangulaud();
-        laud.lisaNupp(2, arvuti.getNupp());
-        laud.valjastaMangulaud();
-        laud.lisaNupp(2, inimene.getNupp());
-        laud.lisaNupp(3, arvuti.getNupp());
-        laud.lisaNupp(3, arvuti.getNupp());
-        laud.lisaNupp(3, inimene.getNupp());
-        laud.lisaNupp(4, arvuti.getNupp());
-        laud.lisaNupp(4, arvuti.getNupp());
-        laud.lisaNupp(4, arvuti.getNupp());
-        laud.lisaNupp(4, inimene.getNupp());
-        laud.valjastaMangulaud();
-        System.out.println(laud.kontrolliVoit(inimene.getNupp()));
+        //mängimine
+        while((laud.kontrolliVoit(inimene.getNupp()) != true) && (laud.kontrolliVoit(arvuti.getNupp()) != true)){
+            System.out.println();
+            System.out.println("Sisestage veerg, kuhu soovite nuppu panna: ");
+            int sisestaVeergu = scanner.nextInt();
+            laud.lisaNupp(sisestaVeergu, inimene.getNupp());
+            System.out.println();
+            System.out.println("Arvuti käik");
+            int suvalineVeerg = ThreadLocalRandom.current().nextInt(1,ridadeArv+1);
+            laud.lisaNupp(suvalineVeerg, arvuti.getNupp());
+            laud.valjastaMangulaud();
+            System.out.println();
+        }
+        if((laud.kontrolliVoit(inimene.getNupp())) == true){
+            System.out.println("Palju õnne " + mangijaNimi + "! Sina võitsid!");
+        }
+        else if ((laud.kontrolliVoit(arvuti.getNupp())) == true){
+            System.out.println("Arvuti võitis!");
+        }
+        else if ((laud.kontrolliVoit(inimene.getNupp()) == false) && (laud.kontrolliVoit(arvuti.getNupp()) == false)){
+            System.out.println("Viik!");
+        }
+        System.out.println("Mäng läbi");
     }
 }
